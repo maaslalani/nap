fefff7d00cbe5ef369ede528eb0e09e5ba349f5f
jolheiser <john.olheiser@gmail.com>
2022-11-21 22:48:53 -0600

    fix: match yaml keys with env var names
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- config.go
+++ config.go
@@ -20,15 +20,15 @@ type Config struct {
 
 	PrimaryColor        string `env:"NAP_PRIMARY_COLOR" envDefault:"#AFBEE1" yaml:"primary_color"`
 	PrimaryColorSubdued string `env:"NAP_PRIMARY_COLOR_SUBDUED" envDefault:"#64708D" yaml:"primary_color_subdued"`
-	BrightGreenColor    string `env:"NAP_BRIGHT_GREEN" envDefault:"#BCE1AF" yaml:"bright_green_color"`
-	GreenColor          string `env:"NAP_GREEN" envDefault:"#527251" yaml:"green_color"`
-	BrightRedColor      string `env:"NAP_BRIGHT_RED" envDefault:"#E49393" yaml:"bright_red_color"`
-	RedColor            string `env:"NAP_RED" envDefault:"#A46060" yaml:"red_color"`
-	ForegroundColor     string `env:"NAP_FOREGROUND" envDefault:"7" yaml:"foreground_color"`
-	BackgroundColor     string `env:"NAP_BACKGROUND" envDefault:"0" yaml:"background_color"`
-	GrayColor           string `env:"NAP_GRAY" envDefault:"240" yaml:"gray_color"`
-	BlackColor          string `env:"NAP_BLACK" envDefault:"#373b41" yaml:"black_color"`
-	WhiteColor          string `env:"NAP_WHITE" envDefault:"#FFFFFF" yaml:"white_color"`
+	BrightGreenColor    string `env:"NAP_BRIGHT_GREEN" envDefault:"#BCE1AF" yaml:"bright_green"`
+	GreenColor          string `env:"NAP_GREEN" envDefault:"#527251" yaml:"green"`
+	BrightRedColor      string `env:"NAP_BRIGHT_RED" envDefault:"#E49393" yaml:"bright_red"`
+	RedColor            string `env:"NAP_RED" envDefault:"#A46060" yaml:"red"`
+	ForegroundColor     string `env:"NAP_FOREGROUND" envDefault:"7" yaml:"foreground"`
+	BackgroundColor     string `env:"NAP_BACKGROUND" envDefault:"0" yaml:"background"`
+	GrayColor           string `env:"NAP_GRAY" envDefault:"240" yaml:"gray"`
+	BlackColor          string `env:"NAP_BLACK" envDefault:"#373b41" yaml:"black"`
+	WhiteColor          string `env:"NAP_WHITE" envDefault:"#FFFFFF" yaml:"white"`
 }
 
 // default helpers for the configuration.
